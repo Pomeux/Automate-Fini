34da3fd5c5afffcad15dbc6d2c3d2b73d20206c0
Planque Léo <134111837+Pomeux@users.noreply.github.com>
2024-04-18 03:21:54 +0200

    Add files via upload

---
+++ automate.ml
@@ -0,0 +1,26 @@
+(*Automate Fini*)
+
+type automate={ 
+  entre:string list;
+  sortie:string list;
+  etat:int list
+
+};;
+
+(*module*)
+
+(*Fonction qui verifie si un mot est compris dans le langage*)
+
+(*Fonction qui retourne l'ensemble *)
+
+(*Fonction qui enleve les e transitions *)
+
+(*Fonction qui traduit un automate non deterministe*)
+
+(*Fonction qui minimise*)
+
+(*Fonction qui  retourne le langage *)
+
+(*Fonction qui retourne une expression rationnelle*)
+
+(*Fonction qui retourne une grammaire de chomsky*)
